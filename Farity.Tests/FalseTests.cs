ace812014d12d00db1bd8b67c8580297a2a4d2f4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-21 15:21:37 +0530

    Add tests for False

---
+++ Farity.Tests/FalseTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace Farity.Tests
+{
+    public class FalseTests
+    {
+        [Fact]
+        public void FalseProvidesFalseGiven0Arguments()
+        {
+            var result = F.False();
+            Assert.Equal(false, result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData("acs")]
+        [InlineData(true)]
+        public void FalseProvidesFalseGiven1Argument(object arg1)
+        {
+            var result = F.False(arg1);
+            Assert.Equal(false, result);
+        }
+
+        [Theory]
+        [InlineData(1, "acs", true)]
+        public void FalseProvidesFalseGivenAnyNumberOfArguments(params object[] args)
+        {
+            var result = F.False(args);
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void FalseProvidesFalseEveryTime()
+        {
+            var result = F.False();
+            Assert.Equal(false, result);
+            var result2 = F.False();
+            Assert.Equal(false, result2);
+        }
+    }
+}
