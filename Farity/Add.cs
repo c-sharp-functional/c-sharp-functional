6f40cf022ae57a2daf601521ac958eb6ea8f714a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-21 19:00:50 +0530

    Add F.Add

---
+++ Farity/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farity
+{
+	public static partial class F
+	{
+		public static decimal Add(decimal a, decimal b) => a + b;
+		public static double Add(double a, double b) => a + b;
+		public static float Add(float a, float b) => a + b;
+		public static int Add(int a, int b) => a + b;
+		public static long Add(long a, long b) => a + b;
+		public static uint Add(uint a, uint b) => a + b;
+		public static ulong Add(ulong a, ulong b) => a + b;
+		public static TimeSpan Add(TimeSpan a, TimeSpan b) => a + b;
+	}
+}
