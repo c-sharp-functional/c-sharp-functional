9c7ab5957827072319b0bfe68d802d72367c7740
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-21 23:26:17 +0530

    Use predefined overloads

--- Farity/Add.cs
+++ Farity/Add.cs
@@ -14,27 +14,27 @@ namespace Farity
         public static TimeSpan Add(TimeSpan a, TimeSpan b) => a + b;
 
         public static decimal? Add(decimal? a, decimal? b)
-            => a == null && b == null ? (decimal?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (decimal?) null : Add(a ?? 0, b ?? 0);
 
         public static double? Add(double? a, double? b)
-            => a == null && b == null ? (double?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (double?) null : Add(a ?? 0, b ?? 0);
 
         public static float? Add(float? a, float? b)
-            => a == null && b == null ? (float?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (float?) null : Add(a ?? 0, b ?? 0);
 
         public static int? Add(int? a, int? b)
-            => a == null && b == null ? (int?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (int?) null : Add(a ?? 0, b ?? 0);
 
         public static long? Add(long? a, long? b)
-            => a == null && b == null ? (long?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (long?) null : Add(a ?? 0, b ?? 0);
 
         public static uint? Add(uint? a, uint? b)
-            => a == null && b == null ? (uint?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (uint?) null : Add(a ?? 0, b ?? 0);
 
         public static ulong? Add(ulong? a, ulong? b)
-            => a == null && b == null ? (ulong?) null : (a ?? 0) + (b ?? 0);
+            => a == null && b == null ? (ulong?) null : Add(a ?? 0, b ?? 0);
 
         public static TimeSpan? Add(TimeSpan? a, TimeSpan? b)
-            => a == null && b == null ? (TimeSpan?) null : (a ?? TimeSpan.Zero) + (b ?? TimeSpan.Zero);
+            => a == null && b == null ? (TimeSpan?) null : Add(a ?? TimeSpan.Zero, b ?? TimeSpan.Zero);
     }
 }
