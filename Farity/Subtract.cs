e854ad954f5170417343baf2068809b1036cf40d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-21 23:26:38 +0530

    Format document

--- Farity/Subtract.cs
+++ Farity/Subtract.cs
@@ -14,16 +14,16 @@ namespace Farity
         public static TimeSpan Subtract(TimeSpan a, TimeSpan b) => a - b;
 
         public static decimal? Subtract(decimal? a, decimal? b)
-            => a == null && b == null ? (decimal?) null : Subtract(a ?? 0,b ?? 0);
+            => a == null && b == null ? (decimal?) null : Subtract(a ?? 0, b ?? 0);
 
         public static double? Subtract(double? a, double? b)
-            => a == null && b == null ? (double?) null : Subtract(a ?? 0,b ?? 0);
+            => a == null && b == null ? (double?) null : Subtract(a ?? 0, b ?? 0);
 
         public static float? Subtract(float? a, float? b)
             => a == null && b == null ? (float?) null : Subtract(a ?? 0, b ?? 0);
 
         public static int? Subtract(int? a, int? b)
-            => a == null && b == null ? (int?) null : Subtract(a ?? 0,b ?? 0);
+            => a == null && b == null ? (int?) null : Subtract(a ?? 0, b ?? 0);
 
         public static long? Subtract(long? a, long? b)
             => a == null && b == null ? (long?) null : Subtract(a ?? 0, b ?? 0);
