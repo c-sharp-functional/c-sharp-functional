629018787f395f5a796360f588bb0fe42d14c181
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-22 12:33:16 +0530

    Fix description for 42

--- Farity.Tests/AlwaysTests.cs
+++ Farity.Tests/AlwaysTests.cs
@@ -7,13 +7,13 @@ namespace Farity.Tests
         [Fact]
         public void AlwaysReturnsAFunctionThatReturnsTheSameValueAlways()
         {
-            var alwaysAnswerToTheUniverseLifeAndEverything = F.Always(42);
-            var answer = alwaysAnswerToTheUniverseLifeAndEverything();
-            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything());
-            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(1));
-            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything("string", null));
-            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(null, "str", 3));
-            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(null, null, null, null));
+            var answerToLifeUniverseAndEverything = F.Always(42);
+            var answer = answerToLifeUniverseAndEverything();
+            Assert.Equal(answer, answerToLifeUniverseAndEverything());
+            Assert.Equal(answer, answerToLifeUniverseAndEverything(1));
+            Assert.Equal(answer, answerToLifeUniverseAndEverything("string", null));
+            Assert.Equal(answer, answerToLifeUniverseAndEverything(null, "str", 3));
+            Assert.Equal(answer, answerToLifeUniverseAndEverything(null, null, null, null));
         }
     }
 }
