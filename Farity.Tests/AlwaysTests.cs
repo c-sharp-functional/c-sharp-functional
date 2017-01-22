093fa19f16ce3f49383f8f49dff546bed976946a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-22 12:32:24 +0530

    Add tests for F.Always

---
+++ Farity.Tests/AlwaysTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Farity.Tests
+{
+    public class AlwaysTests
+    {
+        [Fact]
+        public void AlwaysReturnsAFunctionThatReturnsTheSameValueAlways()
+        {
+            var alwaysAnswerToTheUniverseLifeAndEverything = F.Always(42);
+            var answer = alwaysAnswerToTheUniverseLifeAndEverything();
+            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything());
+            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(1));
+            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything("string", null));
+            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(null, "str", 3));
+            Assert.Equal(answer, alwaysAnswerToTheUniverseLifeAndEverything(null, null, null, null));
+        }
+    }
+}
