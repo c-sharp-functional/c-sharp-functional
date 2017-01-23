dbb748a0052e249e9f3c9950c9473b6ceebab5d2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:05:34 +0530

    Add comments

--- Farity/ToList.cs
+++ Farity/ToList.cs
@@ -5,6 +5,12 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Converts the source into a read only list.
+        /// </summary>
+        /// <typeparam name="T">The type of element in the list.</typeparam>
+        /// <param name="source">The source of items.</param>
+        /// <returns>A read-only list consisting of the elements in the source.</returns>
         public static IReadOnlyList<T> ToList<T>(IEnumerable<T> source) => source.ToImmutableList();
     }
 }
