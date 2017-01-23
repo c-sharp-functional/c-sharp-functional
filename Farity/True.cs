9b356ce75420384fca938c9e8f06114bf4a16583
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:11:08 +0530

    Add comments for F.True

--- Farity/True.cs
+++ Farity/True.cs
@@ -2,6 +2,11 @@
 {
     public static partial class F
     {
+        /// <summary>
+        /// A function that takes any arguments and returns true.
+        /// </summary>
+        /// <param name="args">Any arguments passed to the function.</param>
+        /// <returns>true</returns>
         public static bool True(params object[] args) => true;
     }
 }
