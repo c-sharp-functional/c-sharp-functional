3e4acb9140deec1c13d557ccf398af318114fde2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:09:05 +0530

    Add comments

--- Farity/F.cs
+++ Farity/F.cs
@@ -1,7 +1,13 @@
 ﻿namespace Farity
 {
+    /// <summary>
+    /// The root static class of Farity, through which all of its functional bliss is exposed.
+    /// </summary>
     public static partial class F
     {
+        /// <summary>
+        /// A placeholder used to provide dummy arguments to a function in order to place arguments properly.
+        /// </summary>
         public static readonly Placeholder _ = new Placeholder();
     }
 }
