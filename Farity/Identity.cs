f8e555b1eb4af1c347b609b7a44e35b81a9ce33f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:15:01 +0530

    Add comments

--- Farity/Identity.cs
+++ Farity/Identity.cs
@@ -2,6 +2,13 @@
 {
     public static partial class F
     {
+        /// <summary>
+        /// A function that returns the same value that it was provided.
+        /// </summary>
+        /// <typeparam name="T">The type of value provided.</typeparam>
+        /// <param name="x">The value provided.</param>
+        /// <returns>The same value that was provided to the function.</returns>
+        /// <remarks>Category: Function</remarks>
         public static T Identity<T>(T x) => x;
     }
 }
