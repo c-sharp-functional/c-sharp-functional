31c87887a0a7657270351f3c1014b5471b568c27
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:11:16 +0530

    Add comments for F.False

--- Farity/False.cs
+++ Farity/False.cs
@@ -2,6 +2,11 @@
 {
     public static partial class F
     {
+        /// <summary>
+        /// A function that takes any arguments and returns false.
+        /// </summary>
+        /// <param name="args">Any arguments passed to the function.</param>
+        /// <returns>false</returns>
         public static bool False(params object[] args) => false;
     }
 }
