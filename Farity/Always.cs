b5aeb9b4c7d55019231c7027603043ee38cc4665
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:07:01 +0530

    Add comments

--- Farity/Always.cs
+++ Farity/Always.cs
@@ -2,6 +2,13 @@
 {
     public static partial class F
     {
+        /// <summary>
+        /// Returns a function that takes any arguments but always returns the specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of value to always return.</typeparam>
+        /// <param name="value">The value that is to be always returned.</param>
+        /// <returns>A function that takes any arguments but always return the specified value.</returns>
+        /// <remarks>Category: Function</remarks>
         public static FuncAny<T> Always<T>(T value) => args => value;
     }
 }
