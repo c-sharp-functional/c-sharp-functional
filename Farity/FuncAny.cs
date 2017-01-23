e5e201ba043c13c7ef6e019c35f34afb515f5ae4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-23 23:13:24 +0530

    Add comments

--- Farity/FuncAny.cs
+++ Farity/FuncAny.cs
@@ -1,4 +1,10 @@
 ﻿namespace Farity
 {
+    /// <summary>
+    /// A delegate that represents a function that takes any number of arguments but always returns a specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of value that the function returns.</typeparam>
+    /// <param name="args">Any arguments passed into the function.</param>
+    /// <returns>A value of type <seealso cref="T"/>.</returns>
     public delegate T FuncAny<out T>(params object[] args);
 }
