00a0a88909acb216ed884b3982aef036ebb7b607
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-24 22:19:22 +0530

    Add tests for F.ForEach

---
+++ Farity.Tests/ForEachTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class ForEachTests
+    {
+        [Fact]
+        public void ForEachExecutesFunctionsForAllElementsInTheList()
+        {
+            var expected = "12345";
+            var collected = "";
+            var source = new List<int>() {1, 2, 3, 4, 5};
+            Action<int> action = number => collected += number.ToString();
+            F.ForEach(action, source);
+            Assert.Equal(expected, collected);
+        }
+    }
+}
