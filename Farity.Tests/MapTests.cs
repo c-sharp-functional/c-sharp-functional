a5026cf1aae8d465499a5281402a716c5c7c7215
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-27 13:56:30 +0530

    Add tests for F.Map

---
+++ Farity.Tests/MapTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class MapTests
+    {
+        [Fact]
+        public void MapExecutesFunctionsForAllElementsInTheList()
+        {
+            var expected = new[] {"1", "2", "3", "4", "5"};
+            var source = new[] { 1, 2, 3, 4, 5 };
+            var actual = F.Map(x => x.ToString(), source);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
