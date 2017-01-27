327466e72bea94227ee5bb74c28596173f6b1019
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-27 13:07:34 +0530

    Add tests for F.Tap

---
+++ Farity.Tests/TapTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class TapTests
+    {
+        [Fact]
+        public void TapRunsFunctionOverValue()
+        {
+            var value = "";
+            Action<int> function = number => value = number.ToString();
+            F.Tap(function, 42);
+            Assert.Equal("42", value);
+        }
+
+        [Fact]
+        public void TapReturnsOriginalValue()
+        {
+            const int expected = 42;
+            Action<int> function = number => Debug.WriteLine(number);
+            var result = F.Tap(function, expected);
+            Assert.Equal(expected, result);
+        }
+    }
+}
