8aa43d9f352c3486d565141dc3fda65e2c77f517
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-27 13:07:28 +0530

    Add F.Tap

---
+++ Farity/Tap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Farity
+{
+    public static partial class F
+    {
+        /// <summary>
+        /// Runs the given function with the supplied value, then returns the value.
+        /// Functions that do not mutate the value provided are recommended.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="function">The function to run on the value.</param>
+        /// <param name="value">The value to be provided to the function.</param>
+        /// <returns>The original value supplied.</returns>
+        /// <remarks>Category: Function</remarks>
+        public static T Tap<T>(Action<T> function, T value)
+        {
+            function(value);
+            return value;
+        }        
+    }
+}
