fde5a3ff00bb24aaee9ba08dc4ba015d06770652
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-27 14:01:28 +0530

    Use only IReadOnlyList<T> as source for F.TakeFrom
    in order to improve clarity of intent

--- Farity/TakeFrom.cs
+++ Farity/TakeFrom.cs
@@ -9,22 +9,14 @@ namespace Farity
         /// </summary>
         /// <typeparam name="T">The type of elements in the source.</typeparam>
         /// <param name="index">The index from which to start taking the elements.</param>
-        /// <param name="source">The source of the elements.</param>
+        /// <param name="list">The list of elements.</param>
         /// <returns>The contiguous elements of the source, starting from the specified index.</returns>
         /// <remarks>Category: List</remarks>
-        public static IEnumerable<T> TakeFrom<T>(int index, IEnumerable<T> source)
+        public static IEnumerable<T> TakeFrom<T>(int index, IReadOnlyList<T> list)
         {
-            if (!(source is IReadOnlyList<T>))
-            {
-                var i = 0;
-                foreach (var item in source)
-                    if (index >= i++) yield return item;
-            }
-            var list = (IReadOnlyList<T>)source;
             if (list.Count <= index) yield break;
             for (var i = index; i < list.Count; i++)
                 yield return list[i];
         }
-
     }
 }
