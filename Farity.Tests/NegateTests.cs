c8989fcfe414d1351b33bd3f37d99fa02d7d7614
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-28 23:44:47 +0530

    Add tests for F.Negate

---
+++ Farity.Tests/NegateTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace Farity.Tests
+{
+    public class NegateTests
+    {
+        [Theory]
+        [InlineData(1.22d)]
+        [InlineData(-1.22d)]
+        public void NegateNegatesDouble(double a)
+        {
+            var expected = -a;
+            var actual = F.Negate(a);
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(1.22f)]
+        [InlineData(-1.22f)]
+        public void NegateNegatesFloat(float a)
+        {
+            var expected = -a;
+            var actual = F.Negate(a);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-1)]
+        public void NegateNegatesInteger(int a)
+        {
+            var expected = -a;
+            var actual = F.Negate(a);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1L)]
+        [InlineData(-1L)]
+        public void NegateNegatesLong(long a)
+        {
+            var expected = -a;
+            var actual = F.Negate(a);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
