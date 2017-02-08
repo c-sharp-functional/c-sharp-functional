b0ae1b7c8337d5de453fa55a09c7b52f81a4c997
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-08 19:25:38 +0530

    Add F.Compose for 1 function and arity 1

---
+++ Farity/Compose.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farity
+{
+    public static partial class F
+    {
+        public static
+            Func<T1, TResult>
+            Compose<T1, T2, TResult>(
+                Func<T2, TResult> function2,
+                Func<T1, T2> function1)
+            => a => function2(function1(a));
+    }
+}
