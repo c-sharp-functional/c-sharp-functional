20fb3b78a8d14c8c668228ac25a92d467a00a24f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-22 17:54:57 +0530

    Add tests for F.ReduceWhile

---
+++ Farity.Tests/ReduceWhileTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class ReduceWhileTests
+    {
+        [Fact]
+        public void ReduceWhileExecutesFunctionsForAllElementsInTheList()
+        {
+            var expected = 15;
+            var actual = F.ReduceWhile((r,c) => F.True(r,c), (r, c) => r + c, 0, new[] { 1, 2, 3, 4, 5 });
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReduceWhileExecutesFunctionsTillThePredicateReturnsTrue()
+        {
+            var expected = 10;
+            var actual = F.ReduceWhile((r, c) => r < 9, (r, c) => r + c, 0, new[] { 1, 2, 3, 4, 5 });
+            Assert.Equal(expected, actual);
+        }
+    }
+}
