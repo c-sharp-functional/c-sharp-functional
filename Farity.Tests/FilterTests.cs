70dec69e136e4aff3e8a771b8ebbdbb29f5708a4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-22 20:45:08 +0530

    Add tests for F.Filter

---
+++ Farity.Tests/FilterTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class FilterTests
+    {
+        [Fact]
+        public void FilterFiltersElementsInTheList()
+        {
+            var expected = new[] {1, 2, 3};
+            var actual = F.Filter(x => x < 4, new[] {1, 2, 3, 4, 5});
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterWorksWithTrueAndFalse()
+        {
+            var expected = new[] { 1, 2, 3, 4, 5 };
+            var actual = F.Filter(F.True, new[] { 1, 2, 3, 4, 5 });
+            Assert.Equal(expected, actual);
+            expected = new int[] {};
+            actual = F.Filter(F.False, new[] { 1, 2, 3, 4, 5 });
+            Assert.Equal(expected, actual);
+        }
+    }
+}
