10ec3eab5b811e634fddcc0ea9c0345c378d32cf
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-22 16:04:30 +0530

    Add tests for F.Reduce

---
+++ Farity.Tests/ReduceTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class ReduceTests
+    {
+        [Fact]
+        public void ReduceExecutesFunctionsForAllElementsInTheList()
+        {
+            var expected = 15;
+            var actual = F.Reduce((sum, current) => sum + current, 0, new[] { 1, 2, 3, 4, 5 });
+            Assert.Equal(expected, actual);
+        }
+    }
+}
