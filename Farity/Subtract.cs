89aeed7a8eb7c184c310570325c9d74f799c4dc2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 14:50:04 +0530

    Add comments for F.Subtract

--- Farity/Subtract.cs
+++ Farity/Subtract.cs
@@ -4,6 +4,13 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Subtracts a numeric value from another.
+        /// </summary>
+        /// <typeparam name="T">The data type of the operands in the subtract operation.</typeparam>
+        /// <param name="a">The value from which to subtract.</param>
+        /// <param name="b">The value to be subtracted.</param>
+        /// <returns>The result of the subtraction operation.</returns>
         public static T Subtract<T>(T a, T b)
             where T : struct,
             IComparable,
