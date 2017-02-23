29291f72545a2636b95f4e0f637855f6fa9835ba
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 11:49:46 +0530

    Add tests for F.Property
    Fix #7

---
+++ Farity.Tests/PropertyTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class PropertyTests
+    {
+        [Fact]
+        public void PropertyReturnsPropertyOfObject()
+        {
+            const int expected = 2;
+            var data = new List<int> {1, 2};
+            var countProperty = F.Property<List<int>, int>(list => list.Count);
+            var actual = countProperty(data);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PropertyReturnsReturnValueOfMethodOfObject()
+        {
+            const int expected = 2;
+            var data = new List<int> { 1, 2 };
+            var countProperty = F.Property<IEnumerable<int>, int>(list => list.Count());
+            var actual = countProperty(data);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
