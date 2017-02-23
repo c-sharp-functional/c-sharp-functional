14a9d0f7d6efa936986749a09b8b91640086961d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 15:07:31 +0530

    Add comments for F.Decrement

--- Farity/Decrement.cs
+++ Farity/Decrement.cs
@@ -4,6 +4,12 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Decrements a numeric value by 1.
+        /// </summary>
+        /// <typeparam name="T">The type of the numeric value.</typeparam>
+        /// <param name="value">The value to decrement.</param>
+        /// <returns>The value decremented by 1.</returns>
         public static T Decrement<T>(T value)
             where T : struct,
             IComparable,
