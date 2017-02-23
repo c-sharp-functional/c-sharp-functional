b42f54addb51ea9ed3ab8154be5eb2bd849eaa14
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 14:49:45 +0530

    Add comments for F.Add

--- Farity/Add.cs
+++ Farity/Add.cs
@@ -4,6 +4,13 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Adds two numeric values.
+        /// </summary>
+        /// <typeparam name="T">The data type of the operands in the add operation.</typeparam>
+        /// <param name="a">The addendum.</param>
+        /// <param name="b">The additive.</param>
+        /// <returns>The result of adding the two values.</returns>
         public static T Add<T>(T a, T b)
             where T : struct,
             IComparable,
