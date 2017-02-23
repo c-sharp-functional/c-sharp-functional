497649288e1d9481af5836c9d159ddb2689c7c47
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 14:59:38 +0530

    Add comments for F.Curry upto arity 4

--- Farity/Curry.cs
+++ Farity/Curry.cs
@@ -4,18 +4,45 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Returns the curried version of a function with two arguments.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first argument to the function.</typeparam>
+        /// <typeparam name="T2">The type of the second argument to the function.</typeparam>
+        /// <typeparam name="TResult">The type of result of the function.</typeparam>
+        /// <param name="function">The function to be curried.</param>
+        /// <returns>The curried function representing the provided function.</returns>
         public static
             FuncCurry<T1, T2, TResult>
             Curry<T1, T2, TResult>(
                 Func<T1, T2, TResult> function)
             => a => b => function(a, b);
 
+        /// <summary>
+        /// Returns the curried version of a function with three arguments.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first argument to the function.</typeparam>
+        /// <typeparam name="T2">The type of the second argument to the function.</typeparam>
+        /// <typeparam name="T3">The type of the third argument to the function.</typeparam>
+        /// <typeparam name="TResult">The type of result of the function.</typeparam>
+        /// <param name="function">The function to be curried.</param>
+        /// <returns>The curried function representing the provided function.</returns>
         public static
             FuncCurry<T1, T2, T3, TResult>
             Curry<T1, T2, T3, TResult>(
                 Func<T1, T2, T3, TResult> function)
             => a => b => c => function(a, b, c);
 
+        /// <summary>
+        /// Returns the curried version of a function with four arguments.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first argument to the function.</typeparam>
+        /// <typeparam name="T2">The type of the second argument to the function.</typeparam>
+        /// <typeparam name="T3">The type of the third argument to the function.</typeparam>
+        /// <typeparam name="T4">The type of the fourth argument to the function.</typeparam>
+        /// <typeparam name="TResult">The type of result of the function.</typeparam>
+        /// <param name="function">The function to be curried.</param>
+        /// <returns>The curried function representing the provided function.</returns>
         public static
             FuncCurry<T1, T2, T3, T4, TResult>
             Curry<T1, T2, T3, T4, TResult>(
