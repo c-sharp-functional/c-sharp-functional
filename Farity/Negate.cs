fb0eadd1e63d9fe56da331f1332d33f9d0f1f202
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-23 14:49:56 +0530

    Add comments for F.Negate

--- Farity/Negate.cs
+++ Farity/Negate.cs
@@ -4,6 +4,12 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Negates a numeric value. Does not work with unsigned types.
+        /// </summary>
+        /// <typeparam name="T">The data type of the value to negate.</typeparam>
+        /// <param name="n">The value to be negated.</param>
+        /// <returns>The result of negating the provided value.</returns>
         public static T Negate<T>(T n)
             where T : struct,
             IComparable,
