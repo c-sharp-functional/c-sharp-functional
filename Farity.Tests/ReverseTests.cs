aa47524fe6abeea3ce7897604826d6b09f634e3b
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 18:13:37 +0530

    Add tests for F.Reverse

---
+++ Farity.Tests/ReverseTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System.Linq;
+
+namespace Farity.Tests
+{
+    public class ReverseTests
+    {
+        [Fact]
+        public void ReverseInvertsASequence()
+        {
+            var data = new[] { 1, 2, 3, 4, 5 };
+            var expected = new[] { 5, 4, 3, 2, 1 };
+            var actual = F.Reverse(data);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReverseDoesNotMutateOriginalSequence()
+        {
+            var data = new[] { 1, 2, 3, 4, 5 };
+            var expected = new[] { 1, 2, 3, 4, 5 };
+            var result = F.Reverse(data).ToList();
+            Assert.Equal(expected, data);
+        }
+    }
+}
