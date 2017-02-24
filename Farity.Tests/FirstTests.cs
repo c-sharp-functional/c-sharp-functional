b146e0f97884bbfbf9859ab13fb57ee8a1e7bffc
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 18:02:59 +0530

    Add test for F.First

---
+++ Farity.Tests/FirstTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Linq;
+
+namespace Farity.Tests
+{
+    public class FirstTests
+    {
+        [Fact]
+        public void FirstReturnsFirstElement()
+        {
+            var data = new[] { 1, 2, 3, 4, 5 };
+            var expected = 1;
+            var actual = F.First(data);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
