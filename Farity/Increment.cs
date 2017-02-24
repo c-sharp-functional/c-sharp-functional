891bd0278b7977244110ceadb711b067fa6717b2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 17:21:46 +0530

    Add comments to F.Increment

--- Farity/Increment.cs
+++ Farity/Increment.cs
@@ -4,6 +4,12 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Increments a numeric value by 1.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to increment.</typeparam>
+        /// <param name="value">The value to increment.</param>
+        /// <returns>The provided value incremented by 1.</returns>
         public static T Increment<T>(T value)
             where T : struct,
             IComparable,
