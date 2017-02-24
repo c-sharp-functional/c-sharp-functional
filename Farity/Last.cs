d84c3ccbb79c37fbf069b5f8eb1323a76b82c16c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 17:16:43 +0530

    Refactor out F.Last into separate source file

---
+++ Farity/Last.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Farity
+{
+    public static partial class F
+    {
+        /// <summary>
+        /// Returns the last element of a sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of elemenets in the sequence.</typeparam>
+        /// <param name="source">The sequence over which to operate.</param>
+        /// <returns>The last element of the provided sequence.</returns>
+        public static T Last<T>(IEnumerable<T> source) => source.Last();
+    }
+}
