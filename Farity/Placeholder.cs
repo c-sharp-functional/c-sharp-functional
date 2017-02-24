3546f8237dc044e821a8659ff9786a4c880cc6f7
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 17:24:27 +0530

    Add comments to Farity.Placeholder

--- Farity/Placeholder.cs
+++ Farity/Placeholder.cs
@@ -1,7 +1,14 @@
 ﻿namespace Farity
 {
+    /// <summary>
+    /// A placeholder used to provide dummy arguments to a function in order to place arguments properly
+    /// in the case of partial application of a function.
+    /// </summary>
     public class Placeholder
     {
+        /// <summary>
+        /// Returns a new <seealso cref="Placeholder"/> instance.
+        /// </summary>
         internal Placeholder()
         {
         }
