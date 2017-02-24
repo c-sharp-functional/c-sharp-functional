56061a08b44367f548b8d75f5e2300da3f8f2540
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 19:01:04 +0530

    Add tests for F.Unique

---
+++ Farity.Tests/UniqueTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Farity.Tests
+{
+    public class UniqueTests
+    {
+        [Fact]
+        public void UniqueReturnsUniqueElementsOfList()
+        {
+            var data = new[] { 1, 2, 1, 4, 2 };
+            var expected = new[] { 1, 2, 4 };
+            var actual = F.Unique(data);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UniqueDoesNotMutateOriginalSequence()
+        {
+            var data = new[] { 1, 2, 1, 4, 2 };
+            var expected = new[] { 1, 2, 1, 4, 2 };
+            var result = F.Unique(data);
+            Assert.Equal(expected, data);
+        } 
+    }
+}
