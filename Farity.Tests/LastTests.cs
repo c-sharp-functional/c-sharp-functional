869ac24ae8e29cf4a101e68c798fdc636fe1b656
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 18:05:38 +0530

    Add test for F.Last

---
+++ Farity.Tests/LastTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Linq;
+
+namespace Farity.Tests
+{
+    public class LastTests
+    {
+        [Fact]
+        public void LastReturnsLastElement()
+        {
+            var data = new[] { 1, 2, 3, 4, 5 };
+            var expected = 5;
+            var actual = F.Last(data);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
