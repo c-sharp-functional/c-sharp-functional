a9cce0813af720bce58ce26dceb13f005deb5a27
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 17:27:09 +0530

    Add comments for F.Range

--- Farity/Range.cs
+++ Farity/Range.cs
@@ -5,6 +5,12 @@ namespace Farity
 {
     public static partial class F
     {
+        /// <summary>
+        /// Returns a new sequence of integral numbers within a specified range.
+        /// </summary>
+        /// <param name="start">The first number in the expected sequence.</param>
+        /// <param name="end">The last number in the expected sequence.</param>
+        /// <returns>A new sequence of integral numbers within the specified range.</returns>
         public static IEnumerable<int> Range(int start, int end)
             => start > end ? Enumerable.Empty<int>() : Enumerable.Range(start, end - start + 1);
     }
