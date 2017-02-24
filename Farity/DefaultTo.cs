20cd362ce266a9957b6c03ccc4bc5b8484a244fd
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 16:02:31 +0530

    Refactor out F.DefaultTo into a separate source file

---
+++ Farity/DefaultTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Farity
+{
+    public static partial class F
+    {
+        /// <summary>
+        /// Returns the elements of the specified sequence or the specified value in a singleton collection if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequence.</typeparam>
+        /// <param name="value">The default value.</param>
+        /// <param name="source">The sequence to operate on.</param>
+        /// <returns>The elements of the specified sequence, or the specified value in a singleton collection if the sequence is empty.</returns>
+        public static IEnumerable<T> DefaultTo<T>(T value, IEnumerable<T> source) => source.DefaultIfEmpty(value);
+    }
+}
--- Farity/Enumerable.cs
+++ Farity/Enumerable.cs
@@ -5,8 +5,6 @@ namespace Farity
 {
     public static partial class F
     {
-        public static IEnumerable<T> DefaultTo<T>(T value, IEnumerable<T> source) => source.DefaultIfEmpty(value);
-
         public static T First<T>(IEnumerable<T> source) => source.First();
 
         public static T Last<T>(IEnumerable<T> source) => source.Last();
