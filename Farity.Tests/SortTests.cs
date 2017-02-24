0aca2667889d6f59f1eb8bce6f98ce4898ddd7ec
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 14:57:57 +0530

    Add tests for F.Sort

---
+++ Farity.Tests/SortTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void SortSortsTheItemsInAscendingOrder()
+        {
+            var expected = new[] {1, 2, 3, 5, 9};
+            var data = new[] {3, 2, 1, 9, 5};
+            var actual = F.Sort(data);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SortDoesNotModifyOriginalSequence()
+        {
+            var expected = new[] { 1, 2, 3, 5, 9 };
+            var data = new[] { 3, 2, 1, 9, 5 };
+            var result = F.Sort(data).ToList();
+            Assert.Equal(expected, new[] { 1, 2, 3, 5, 9 });
+        }
+    }
+}
