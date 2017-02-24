1efc2e47efdb190baad3f1d3eaccc9a57a9fd9f4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-24 18:54:49 +0530

    Add tests for F.ToList

---
+++ Farity.Tests/ToListTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using Xunit;
+
+namespace Farity.Tests
+{
+    public class ToListTests
+    {
+        [Fact]
+        public void ToListReturnsImmutableList()
+        {
+            var data = new[] { 1, 2, 3, 4, 5 };
+            var actual = F.ToList(data);
+            Assert.IsType<ImmutableList<int>>(actual);
+        }
+
+
+        [Fact]
+        public void ToListContainsSameItemsAsProvidedSequence()
+        {
+            var expected = new[] { 1, 2, 3, 4, 5 };
+            var actual = F.ToList(expected);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
